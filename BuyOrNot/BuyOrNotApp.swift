4c64f2d858dcf16d389ba26061848e0771965f67
EGC-TB <147960707+EGC-TB@users.noreply.github.com>
2025-11-07 11:39:46 -0500

    Initial Commit

---
+++ BuyOrNot/BuyOrNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyOrNotApp.swift
+//  BuyOrNot
+//
+//  Created by Eagle Chen on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BuyOrNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuyOrNotTests/BuyOrNotTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuyOrNotTests.swift
+//  BuyOrNotTests
+//
+//  Created by Eagle Chen on 11/7/25.
+//
+
+import Testing
+@testable import BuyOrNot
+
+struct BuyOrNotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
